29b5358926096d5bb18c0bd83d41f0c738d20255
Steven Song <steven.song98@gmail.com>
2024-11-19 15:28:29 -0600

    anonymized

--- run-evals.sh
+++ run-evals.sh
@@ -1,41 +1,41 @@
 export CUDA_VISIBLE_DEVICES=1
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/cxrrepair_impression.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/cxrrepair_impression_METRICS.csv
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/cxrredone_impression.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/cxrredone_impression_METRICS.csv
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/xrem_impression.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/xrem_impression_METRICS.csv
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/cxrmate_impression.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/cxrmate_impression_METRICS.csv
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/chexagent_impression.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/chexagent_impression_METRICS.csv
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/cxrmate_findings.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/cxrmate_findings_METRICS.csv
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/chexagent_findings.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/chexagent_findings_METRICS.csv
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/rgrg_findings.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/rgrg_findings_METRICS.csv
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/cxrmate_both.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/cxrmate_both_METRICS.csv
 
-python /opt/gpudata/steven/label-boosted-RAG-for-RRG/rrg/eval.py \
+python /opt/gpudata/label-boosted-RAG-for-RRG/rrg/eval.py \
 --report_csv /opt/gpudata/rrg-data-2/baselines/chexagent_both.csv \
 --output_csv /opt/gpudata/rrg-data-2/baselines/chexagent_both_METRICS.csv

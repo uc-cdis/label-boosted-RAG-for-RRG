72206ccf2fe862657a3f1c15938cc44465939c38
StevenSong <steven.song98@gmail.com>
2025-08-19 08:40:33 -0500

    labrag with opt cls

---
+++ scripts/with-best/3-run-generate-eval-chexpertplus-findings.sh
@@ -0,0 +1,91 @@
+REPO_ROOT=/opt/gpudata/steven/label-boosted-RAG-for-RRG
+MIMIC_CXR_DIR=/opt/gpudata/mimic-cxr
+CHEXPERTPLUS_DIR=/opt/gpudata/chexpertplus
+LABEL_DIR=/opt/gpudata/cxr-derived
+BASE_OUTPUT_DIR=/opt/gpudata/labrag
+
+set -e
+
+# =================================
+# Core Experiments
+# =================================
+
+python $REPO_ROOT/rrg/generate.py \
+--model mistralai/Mistral-7B-Instruct-v0.3 \
+--filter_type no-filter \
+--prompt_type naive \
+--section_type findings \
+--k 5 \
+--batch_size 32 \
+--prompt_yaml $REPO_ROOT/rrg/prompts.yaml \
+--split_csv $CHEXPERTPLUS_DIR/split.csv \
+--metadata_csv $CHEXPERTPLUS_DIR/metadata.csv \
+--true_label_csv $LABEL_DIR/chexpertplus-findings-labels.csv \
+--predicted_label_csv $BASE_OUTPUT_DIR/chexpertplus-findings-best-pred.csv \
+--report_csv $CHEXPERTPLUS_DIR/report.csv \
+--feature_h5 $BASE_OUTPUT_DIR/chexpertplus-gloria.h5 \
+--output_dir $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best
+
+python $REPO_ROOT/rrg/eval.py \
+--report_csv $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best/findings_top-5_chexpertplus-gloria-pred-label_no-filter_naive_Mistral-7B-Instruct-v0.3.csv \
+--output_csv $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best/findings_top-5_chexpertplus-gloria-pred-label_no-filter_naive_Mistral-7B-Instruct-v0.3_METRICS.csv
+
+python $REPO_ROOT/rrg/generate.py \
+--model mistralai/Mistral-7B-Instruct-v0.3 \
+--filter_type exact \
+--prompt_type naive \
+--section_type findings \
+--k 5 \
+--batch_size 32 \
+--prompt_yaml $REPO_ROOT/rrg/prompts.yaml \
+--split_csv $CHEXPERTPLUS_DIR/split.csv \
+--metadata_csv $CHEXPERTPLUS_DIR/metadata.csv \
+--true_label_csv $LABEL_DIR/chexpertplus-findings-labels.csv \
+--predicted_label_csv $BASE_OUTPUT_DIR/chexpertplus-findings-best-pred.csv \
+--report_csv $CHEXPERTPLUS_DIR/report.csv \
+--feature_h5 $BASE_OUTPUT_DIR/chexpertplus-gloria.h5 \
+--output_dir $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best
+
+python $REPO_ROOT/rrg/eval.py \
+--report_csv $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best/findings_top-5_chexpertplus-gloria-pred-label_exact_naive_Mistral-7B-Instruct-v0.3.csv \
+--output_csv $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best/findings_top-5_chexpertplus-gloria-pred-label_exact_naive_Mistral-7B-Instruct-v0.3_METRICS.csv
+
+python $REPO_ROOT/rrg/generate.py \
+--model mistralai/Mistral-7B-Instruct-v0.3 \
+--filter_type no-filter \
+--prompt_type simple \
+--section_type findings \
+--k 5 \
+--batch_size 32 \
+--prompt_yaml $REPO_ROOT/rrg/prompts.yaml \
+--split_csv $CHEXPERTPLUS_DIR/split.csv \
+--metadata_csv $CHEXPERTPLUS_DIR/metadata.csv \
+--true_label_csv $LABEL_DIR/chexpertplus-findings-labels.csv \
+--predicted_label_csv $BASE_OUTPUT_DIR/chexpertplus-findings-best-pred.csv \
+--report_csv $CHEXPERTPLUS_DIR/report.csv \
+--feature_h5 $BASE_OUTPUT_DIR/chexpertplus-gloria.h5 \
+--output_dir $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best
+
+python $REPO_ROOT/rrg/eval.py \
+--report_csv $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best/findings_top-5_chexpertplus-gloria-pred-label_no-filter_simple_Mistral-7B-Instruct-v0.3.csv \
+--output_csv $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best/findings_top-5_chexpertplus-gloria-pred-label_no-filter_simple_Mistral-7B-Instruct-v0.3_METRICS.csv
+
+python $REPO_ROOT/rrg/generate.py \
+--model mistralai/Mistral-7B-Instruct-v0.3 \
+--filter_type exact \
+--prompt_type simple \
+--section_type findings \
+--k 5 \
+--batch_size 32 \
+--prompt_yaml $REPO_ROOT/rrg/prompts.yaml \
+--split_csv $CHEXPERTPLUS_DIR/split.csv \
+--metadata_csv $CHEXPERTPLUS_DIR/metadata.csv \
+--true_label_csv $LABEL_DIR/chexpertplus-findings-labels.csv \
+--predicted_label_csv $BASE_OUTPUT_DIR/chexpertplus-findings-best-pred.csv \
+--report_csv $CHEXPERTPLUS_DIR/report.csv \
+--feature_h5 $BASE_OUTPUT_DIR/chexpertplus-gloria.h5 \
+--output_dir $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best
+
+python $REPO_ROOT/rrg/eval.py \
+--report_csv $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best/findings_top-5_chexpertplus-gloria-pred-label_exact_simple_Mistral-7B-Instruct-v0.3.csv \
+--output_csv $BASE_OUTPUT_DIR/exp-chexpertplus/exp-findings/exp-core-best/findings_top-5_chexpertplus-gloria-pred-label_exact_simple_Mistral-7B-Instruct-v0.3_METRICS.csv
